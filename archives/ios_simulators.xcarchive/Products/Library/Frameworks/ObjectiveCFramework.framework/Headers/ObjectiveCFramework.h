5dd99029a2ef61d0040e2a6218e1840622d0f805
Tran Quang Huy <>
2021-06-21 10:22:16 +0700

    my first commit

---
+++ archives/ios_simulators.xcarchive/Products/Library/Frameworks/ObjectiveCFramework.framework/Headers/ObjectiveCFramework.h
@@ -0,0 +1,18 @@
+//
+//  ObjectiveCFramework.h
+//  ObjectiveCFramework
+//
+//  Created by Tran Quang Huy on 6/21/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjectiveCFramework.
+FOUNDATION_EXPORT double ObjectiveCFrameworkVersionNumber;
+
+//! Project version string for ObjectiveCFramework.
+FOUNDATION_EXPORT const unsigned char ObjectiveCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveCFramework/PublicHeader.h>
+
+
